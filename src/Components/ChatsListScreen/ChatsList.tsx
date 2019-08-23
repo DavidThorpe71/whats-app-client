64a744cec97e98a1d4eb731145e080413b862c83
David Thorpe <davidthorpe878@gmail.com>
2019-08-23 20:40:37 +0100

    update to use antd

--- src/Components/ChatsListScreen/ChatsList.tsx
+++ src/Components/ChatsListScreen/ChatsList.tsx
@@ -1,90 +1,40 @@
 import React, { FC } from "react";
 import {chats} from '../../db';
 import moment from 'moment';
-import { List } from 'antd';
-import { ListProps } from 'antd/lib/list/index'
 import styled from 'styled-components';
+import { Comment, List, Tooltip, Avatar } from 'antd';
 
 const Container = styled.div`
   height: calc(100% - 56px);
   overflow-y: overlay;
 `;
-const StyledList = styled((props: ListProps<{
-  id: string;
-  name: string;
-  picture: string;
-  lastMessage: {
-      id: string;
-      content: string;
-      createdAt: Date;
-  } | undefined;
-}>) => <List {...props} />)`
-  padding: 0 !important;
-`;
-const StyledListItem = styled(props => <List.Item {...props}/>)`
-  height: 76px;
-  padding: 0 15px;
-  display: flex;
-`;
-const ChatPicture = styled.img`
-  height: 50px;
-  width: 50px;
-  object-fit: cover;
-  border-radius: 50%;
-`;
-const ChatInfo = styled.div`
-  width: calc(100% - 60px);
-  height: 46px;
-  padding: 15px 0;
-  margin-left: 10px;
-  border-bottom: 0.5px solid silver;
-  position: relative;
-`;
-const ChatName = styled.div`
-  margin-top: 5px;
-`;
-const MessageContent = styled.div`
-  color: gray;
-  font-size: 15px;
-  margin-top: 5px;
-  text-overflow: ellipsis;
-  overflow: hidden;
-  white-space: nowrap;
-`;
-const MessageDate = styled.div`
-  position: absolute;
-  color: gray;
-  top: 20px;
-  right: 0;
-  font-size: 13px;
-`;
+
 
 const ChatsList: FC = () => (
   <Container>
-    <StyledList
+    <List
       bordered
       dataSource={chats}
       renderItem={item => (
-        <StyledListItem button>
-          <ChatPicture
-          src={item.picture}
-          alt="Profile"
+        <List.Item>
+          <Comment
+            author={item.name}
+            avatar={
+              <Avatar
+                src={item.picture}
+                alt="Profile"
+              />
+            }
+            content={item.lastMessage && item.lastMessage.content}
+            datetime={
+              <Tooltip title={moment(item.lastMessage && item.lastMessage.createdAt).format('HH:mm')}>
+                <span>{moment(item.lastMessage && item.lastMessage.createdAt).fromNow()}</span>
+              </Tooltip>
+            }
           />
-          <ChatInfo>
-
-          <ChatName>{item.name}</ChatName>
-          {item.lastMessage && (
-            <>
-              <MessageContent>{item.lastMessage.content}</MessageContent>
-              <MessageDate>{moment(item.lastMessage.createdAt).format('HH:mm')}</MessageDate>
-            </>
-          )}
-          </ChatInfo>
-        </StyledListItem>
+        </List.Item>
       )}
     />
-
-
   </Container>
 );
 export default ChatsList;
